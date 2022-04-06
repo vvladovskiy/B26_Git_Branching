56cfe2b0572d3e54fc0737eb2b2e303577d4f189
Vick Wladowski <vvladovskiy@gmail.com>
2022-04-06 14:37:36 -0700

    First Commit on Branches Project

--- src/github$multibranches/TestForBranching.java
+++ src/github$multibranches/TestForBranching.java
@@ -1,6 +1,6 @@
-package test;
+package github$multibranches;
 
-public class TestForBrsnchimg {
+public class TestForBranching {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
